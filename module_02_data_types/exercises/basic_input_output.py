aaaebd54834a2b014c62ff319e94d01d74e1111e
Luca Lombardo <lucalombardi336@gmail.com>
2025-03-21 21:03:04 -0300

    Create 'basic_input_output.py', a basic sum calculator that handles sum values by float.

---
+++ module_02_data_types/exercises/basic_input_output.py
@@ -0,0 +1,7 @@
+print("Please enter a value for 'a'")
+a = float(input())
+print("Now enter a value for 'b'")
+b = float(input())
+sum = a + b
+print("The total sum for", a ,"and", b , "is", sum)
+print("That is all, folks!")
